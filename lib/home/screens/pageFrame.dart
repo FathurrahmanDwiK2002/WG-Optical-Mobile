de43b6d7282124ad4330a80bb861fe2496ad192a
mphstar <bintang.simdig20@gmail.com>
2022-12-23 13:12:25 +0700

    Add frame to keranjang

--- lib/home/screens/pageFrame.dart
+++ lib/home/screens/pageFrame.dart
@@ -1,12 +1,21 @@
 // ignore_for_file: camel_case_types, file_names, unnecessary_new, prefer_final_fields, unused_element, non_constant_identifier_names, prefer_const_constructors, unused_local_variable, unused_field, avoid_types_as_parameter_names, prefer_const_literals_to_create_immutables
 
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:wg_optical/home/screens/pageCart.dart';
 import 'package:wg_optical/home/screens/pembayaran.dart';
 import 'package:wg_optical/models/warna.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
+import '../../env/env.dart';
+import '../widget/navbar.dart';
 
 class pageFrame extends StatefulWidget {
   const pageFrame({super.key});
@@ -17,40 +26,68 @@ class pageFrame extends StatefulWidget {
 
 class _pageFrameState extends State<pageFrame>
     with SingleTickerProviderStateMixin {
-  List<Map<String, dynamic>> _Data = [];
-  var _account = Hive.box('Data');
-  final anu = new TextEditingController();
-  final kodeframe = new TextEditingController();
+  final _loadHiveProfile = Hive.box('Profile');
+  List<Map<String, dynamic>> _dataProfile = [];
+  List<dynamic> _dataFrame = [];
+
+  final nominal = new TextEditingController();
   late TabController tabController;
-  Future<void> _addData(Map<String, dynamic> newData) async {
-    await _account.add(newData);
-  }
+  String selectedFrame = '';
+  String minHarga = '';
+
+  final formatter = NumberFormat.simpleCurrency(locale: 'id_ID');
 
   @override
   void initState() {
     tabController = TabController(length: 2, vsync: this);
     super.initState();
-  }
-
-  void load_data() {
-    final data = _account.keys.map((Key) {
-      final value = _account.get(Key);
-      return {
-        "key": Key,
-        "name": value["name"],
-        "kode": value["kode"],
-      };
-    }).toList();
-
-    setState(() {
-      _Data = data.reversed.toList();
-    });
+    _refreshItems();
+    _getFrame();
   }
 
   @override
   void dispose() {
     tabController.dispose();
     super.dispose();
+  }
+
+  void _refreshItems() {
+    final data = _loadHiveProfile.keys.map((key) {
+      final value = _loadHiveProfile.get(key);
+      return {
+        "key": key,
+        "id_pegawai": value["id_pegawai"],
+        "nama": value["nama"],
+        "alamat": value["alamat"],
+        "notelepon": value["notelepon"],
+        "urlFoto": value["urlFoto"],
+      };
+    }).toList();
+
+    setState(() {
+      _dataProfile = data.reversed.toList();
+    });
+  }
+
+  void _getFrame() async {
+    EasyLoading.show(status: 'Loading');
+
+    final respone = await http.post(
+      Uri.parse('${Env.URL_PREFIX}api/menuFrame.php'),
+      body: {
+        "type": "getFrame",
+        "apikey": "aoi12j1h7dwgopticalw1dggwuawdki",
+        'id_pegawai': _dataProfile[0]['id_pegawai'],
+      },
+    );
+
+    setState(() {
+      _dataFrame = jsonDecode(respone.body);
+    });
+
+    if (respone.statusCode == 200) {
+      EasyLoading.dismiss();
+    }
   }
 
   @override
@@ -160,10 +197,43 @@ class _pageFrameState extends State<pageFrame>
                           ),
                           child: Padding(
                             padding: const EdgeInsets.only(left: 20),
-                            child: TextField(
-                              // controller: kodeframe,
-                              decoration: InputDecoration(
-                                hintText: "",
+                            child: DropdownSearch(
+                              onChanged: (value) {
+                                setState(() {
+                                  selectedFrame = value;
+                                  for (var i = 0; i < _dataFrame.length; i++) {
+                                    if (_dataFrame[i]['data']['Id_Bawa'] ==
+                                        value) {
+                                      minHarga =
+                                          'Harga Minimal: ${formatter.format(int.parse(_dataFrame[i]['data']['harga_jual']))}';
+                                    }
+                                  }
+                                });
+                              },
+                              popupProps: PopupProps.dialog(
+                                  title: Container(
+                                    alignment: Alignment.center,
+                                    height: 50,
+                                    child: Text(
+                                      'Pilih Kode Frame',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  searchFieldProps: TextFieldProps(
+                                      decoration: InputDecoration(
+                                          prefixIcon: Icon(Icons.search),
+                                          hintText: 'Cari Kode Frame')),
+                                  fit: FlexFit.loose,
+                                  searchDelay: Duration(seconds: 0),
+                                  showSearchBox: true),
+                              items: _dataFrame.map((data) {
+                                return data['data']['Id_Bawa'];
+                              }).toList(),
+                              dropdownDecoratorProps: DropDownDecoratorProps(
+                                textAlignVertical: TextAlignVertical.center,
+                                dropdownSearchDecoration:
+                                    InputDecoration.collapsed(
+                                        hintText: selectedFrame),
                               ),
                             ),
                           ),
@@ -201,9 +271,11 @@ class _pageFrameState extends State<pageFrame>
                           child: Padding(
                             padding: const EdgeInsets.only(left: 20),
                             child: TextField(
-                              // controller: kodeframe,
+                              keyboardType: TextInputType.number,
+                              controller: nominal,
                               decoration: InputDecoration(
-                                hintText: "",
+                                border: InputBorder.none,
+                                hintText: minHarga,
                               ),
                             ),
                           ),
@@ -239,11 +311,12 @@ class _pageFrameState extends State<pageFrame>
                 ),
                 child: TextButton(
                     onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => pageCart(),
-                          ));
+                      print(_dataFrame);
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //       builder: (context) => pageCart(),
+                      //     ));
                     },
                     child: Text(
                       "Keranjang",
@@ -267,12 +340,46 @@ class _pageFrameState extends State<pageFrame>
                   color: Color(0xffc9cbca),
                 ),
                 child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => pembayaran(),
-                          ));
+                    onPressed: () async {
+                      var respone = await http.post(
+                        Uri.parse('${Env.URL_PREFIX}api/menuFrame.php'),
+                        body: {
+                          "type": "insert",
+                          "apikey": "aoi12j1h7dwgopticalw1dggwuawdki",
+                          'id_pegawai': _dataProfile[0]['id_pegawai'],
+                          "kode_frame": selectedFrame,
+                          "harga": nominal.text,
+                        },
+                      );
+
+                      var listD = jsonDecode(respone.body);
+
+                      showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: ((context) {
+                          return AlertDialog(
+                            title: Text(listD['status']),
+                            content: Text(listD['msg']),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  listD['status'] == 'Berhasil'
+                                      ? Navigator.of(context)
+                                          .pushAndRemoveUntil(
+                                              MaterialPageRoute(
+                                                builder: ((context) =>
+                                                    navbar()),
+                                              ),
+                                              (route) => false)
+                                      : Navigator.of(context).pop();
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        }),
+                      );
                     },
                     child: Text(
                       "Beli",
